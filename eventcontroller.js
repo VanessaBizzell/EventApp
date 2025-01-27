704b080c08007c5f650d258b6349427a42dde306
Vanessa Bizzell <vanessabizzell@gmail.com>
2025-01-27 12:23:20 +0000

    event controller file updated

--- eventcontroller.js
+++ eventcontroller.js
@@ -1,95 +1,79 @@
-// let movies=[];
-// let id = 0;
+
 const createError = require("http-errors");
-const movieDB = require("./Schemas/Movie");
+const eventDB = require("./schemas/Events");
 
-exports.getAllMovies = async (req, res) => {
+exports.getAllEvents = async (req, res) => {
   try {
-    const movies = await movieDB.find();
-    res.send(movies);
+    const events = await eventDB.find();
+    res.send(events);
   } catch (error) {
     next(createError(500, error.message));
   }
 };
 
-exports.createMovie = async (req, res) => {
+exports.createEvent = async (req, res) => {
   try {
-    const { title, watched, director, year } = req.body;
-    const newMovie = await movieDB.create({
-      title,
-      watched: Boolean(watched),
-      director,
-      year,
+    const { name, location, summary, date, time } = req.body;
+    const newEvent = await eventDB.create({
+      name,
+      location,
+     summary,
+      date,
+      time,
     });
-    res.send(newMovie);
+    res.send(newEvent);
   } catch (error) {
     next(createError(500, error.message));
   }
 };
 
-//     const movie = req.body;
-//     movie.id = id;
-//     //push the new movie into the movies array
-//     movies.push(movie);
-//     id++;
-//     //send back the updated array to user
-//     res.send(movies);
-// }
 
-exports.getMovieById = async (req, res, next) => {
+exports.getEventById = async (req, res, next) => {
   const id = req.params.id;
 
   try {
-    const movie = await movieDB.findById(id);
+    const event = await eventDB.findById(id);
+    if (!event) {
+      return next(createError(404, "No event with that id"));
+    }
+    res.send(event);
+  } catch (error) {
+    next(createError(500, error.message));
+  }
+};
+
+
+exports.deleteEventById = async (req, res, next) => {
+  const id = req.params.id;
+  try {
+    const event = await eventDB.findByIdAndDelete(id);
     if (!movie) {
-      return next(createError(404, "No movie with that id"));
+      return next(createError(404, "No event with that id"));
     }
-    res.send(movie);
-  } catch (error) {
-    next(createError(500, error.message));
-  }
-};
-/*
-    
-    const movie = movies.find(movie => movie.id == parseInt(id))
-    if (movie) {
-        res.send(movie)
-    } else {
-        next(createError(404, "Movie not found"));
-    }
-        */
-
-exports.deleteMovie = async (req, res, next) => {
-  const id = req.params.id;
-  try {
-    const movie = await movieDB.findByIdAndDelete(id);
-    if (!movie) {
-      return next(createError(404, "No movie with that id"));
-    }
-    res.send(movie);
+    res.send(event);
   } catch (error) {
     next(createError(500, error.message));
   }
 };
 
-exports.updateMovie = async (req, res, next) => {
-  const { title, watched, director, year } = req.body;
+exports.updateEventById = async (req, res, next) => {
+  const {  name, location, summary, date, time } = req.body;
   const id = req.params.id;
 
-  //   const movie = movies.find((movie) => movie.id == parseInt(id));
 
   try {
-    const movie = await movieDB.findByIdAndUpdate(id, {
-    //  ...movie,
-      title: title,
-      director: director,
-      year: year,
-      watched: watched,
+    const event = await eventDB.findByIdAndUpdate(id, {
+
+      name: name,
+      location: location,
+      summary: summary,
+      date: date,
+      time: time,
     },{new:true});
-    if (!movie) {
-      return res.send("movie not found");
+    if (!event) {
+      return res.send("event not found");
     }
-    res.send(movie);
+    res.send(event);
   } catch (error) {
     next(createError(500, error.message));
   }
